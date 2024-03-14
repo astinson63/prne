b8a9527ae2aceadd3180771dde6c0fb06ec278eb
astinson63 <astinson@firefly.cloud>
2024-03-14 13:06:28 -0500

    example xml

--- example_xml.py
+++ example_xml.py
@@ -4,3 +4,6 @@ with open('example.xml', 'r') as f:
     xml_data = f.read()
     soup = BeautifulSoup(xml_data, features='xml')
     
+print(f'\nDevice\t\tDevice Name\tIP\n{"-"*50}')
+for router in soup.find_all('router'):
+    print(f'Router:\t\t{router["name"]}\t\t{router.ip.string}')
